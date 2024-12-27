9c2121af140b78b89c67a2e62f1db2c190f60740
Anastasiia.Posulikhi <anastasiia.posulikhina@jetbrains.com>
2024-12-27 14:53:20 +0200

    Delete ConfigurationBinderSourceGeneration file

--- TestRoslynInNET9/Generators/NET8/Configuration.Binder.SourceGeneration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TestRoslynInNET8.Generators;
-
-public class ConfigurationBinderSourceGeneration
-{
-    // EXPECTED: Bind method generated with a source generator is used
-    internal void Configure(IConfiguration configuration) =>
-        ConfigurationBinder.Bind(configuration: configuration, instance: this);
-}
